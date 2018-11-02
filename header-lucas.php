acc0fc2f28443532dfbece9b80d7e51efbae0e53
seamaster02 <mantoani.lucas@stagiairesifa.fr>
2018-11-02 10:18:15 +0100

    modifications lucas

---
+++ header-lucas.php
@@ -0,0 +1,16 @@
+<style>
+        header {
+            text-align: center;
+        }
+        nav     {
+            text-align: center;
+        }
+
+</style>
+
+<header>
+    <h1> Catalogue Retro Gaming
+</header>
+<nav>
+    barre de navigation
+</nav>
