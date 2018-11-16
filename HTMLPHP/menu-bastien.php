325ea03ddbd4ca4c7f00ff2ba4f6e9842ce3ce1a
Lerinai <mantes.bastien@stagiairesifa.fr>
2018-11-16 10:58:59 +0100

    modif

--- HTMLPHP/menu-bastien.php
+++ HTMLPHP/menu-bastien.php
@@ -7,7 +7,9 @@
     <title>Page Title</title>
 </head>
 <body>
+    <header>
     <?php include("header-lucas.html"); ?>
+    </header>
 
 <table class="tableau">
 <?php
@@ -20,7 +22,7 @@ $chemin = '../DONNEES';
                                                                                 // Ouverture du répertoire
 $repertoire = opendir($chemin);
 
-function nb_element($repertoire, $nbFichiers)
+function nb_element($repertoire)
 {
     $nbFichiers = 0;
     while($fichier = readdir($repertoire))
@@ -31,11 +33,13 @@ function nb_element($repertoire, $nbFichiers)
             }
         }
     return $nbFichiers;
-    }
+}
+
+$id = nb_element($repertoire);
 
 //fopen("DONNEES/F".nb_element($repertoire, $nbFichiers).".txt", "w");
 
-for($i=0; $i<5; $i++)
+for($i=0; $i<$id; $i++)
 {
 $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
 
@@ -45,10 +49,12 @@ $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
     $line_of_text = fgets($file_handle);
     $gamedetail = explode("#", $line_of_text);
     echo"<tr>";
-
-        for($j=0; $j<count($gamedetail); $j++)
-        {
-            echo"<td>$gamedetail[$j]</td>";
+        if($gamedetail[5]==1)
+        {    
+            for($j=0; $j<(count($gamedetail)-2); $j++)
+            {
+                echo"<td><a href='detail.php?id=" . $gamedetail[0] . "'>" . $gamedetail[$j] . "</a></td>";
+            }
         }
 
     echo"</tr>";
@@ -57,21 +63,9 @@ $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
     fclose($file_handle);
 }
 
-echo nb_element($repertoire, $nbFichiers);
 ?>
 
 </table>
-
-<form method='post'>
-
-    <input type='text' name='ID' value='0'/>
-    <input type='text' name='Titre' value='Titre'/>
-    <input type='file' name='Image' value='DL_image' accept="image/png, image/jpeg, image/jpg"/>
-    <input type='date' name='Date de sortie' value='jj/mm/aaaa'/>
-    <input type='text' name='Description' value='Résumé du jeu'/>
-    <input type='submit' name='sub1'/>
-
-</form>
 
 </body>
 </html>
