c6965cc59caac1761098aa4e6a7e42d877f17fca
Chouchii <barthel.amelie@stagiairesifa.fr>
2018-11-16 11:11:45 +0100

    lol

--- HTMLPHP/supernintendo.php
+++ HTMLPHP/supernintendo.php
@@ -1,5 +1,41 @@
-<?php  
-for($i=0; $i<$id; $i++)
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="../CSS/catalogue.css">
+    <title>Catalogue RetroGaming</title>
+</head>
+<body>
+    <?php include("header-lucas.html"); ?>
+
+<table class="tableau">
+<?php
+
+$nbFichiers = 0;
+
+
+                                                                                // Choix du répertoire
+$chemin = '../DONNEES';
+                                                                                // Ouverture du répertoire
+$repertoire = opendir($chemin);
+
+function nb_element($repertoire, $nbFichiers)
+{
+    $nbFichiers = 0;
+    while($fichier = readdir($repertoire))
+        {
+            if ($fichier != "." && $fichier != "..")
+            {
+                $nbFichiers++;
+            }
+        }
+    return $nbFichiers;
+    }
+
+//fopen("DONNEES/F".nb_element($repertoire, $nbFichiers).".txt", "w");
+
+for($i=0; $i<$nbFichiers; $i++)
 {
 $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
 
@@ -9,7 +45,7 @@ $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
     $line_of_text = fgets($file_handle);
     $gamedetail = explode("#", $line_of_text);
     echo"<tr>";
-        if($gamedetail[5]==1)
+        if($gamedetail[5]=="sn")
         {    
             for($j=0; $j<(count($gamedetail)-2); $j++)
             {
@@ -22,5 +58,9 @@ $file_handle = fopen("../DONNEES/F".$i.".txt", "r");
 
     fclose($file_handle);
 }
-
 ?>
+</table>
+
+</body>
+</html>
+
