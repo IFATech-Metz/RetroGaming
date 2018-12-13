49e2f9f7112400cc2f03afa70798238f8896fc88
Lerinai <mantes.bastien@stagiairesifa.fr>
2018-12-13 15:17:23 +0100

    modif

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
         <meta charset="utf-8">
         <title></title>
     </head>
+    <?php include("HTMLPHP/header.php"); ?>
     <body style="background-image:url(IMAGES/fond.jpg)";>
            <p> Bonjour et bienvenue sur le catalogue RetroGaming, vous allez pouvoir retrouver les jeux de votre enfance! Et même le completer avec vos jeux préfèrés ! <br>
            Have fun ! </p>
 
-           <p> <a href="HTMLPHP/menu.php"> Cliquez ici pour visiter le catalogue</a></p>
            <img id="retro" src="IMAGES/retrogaming.jpg" alt="photo accueil"> >
     </body>
 </html>
