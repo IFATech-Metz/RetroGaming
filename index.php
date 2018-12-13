e488601ce00c137fb3eef6e9f63775b4bd0f03f0
Lerinai <mantes.bastien@stagiairesifa.fr>
2018-12-13 15:02:47 +0100

    Last update

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
         <title></title>
     </head>
     <body>
-        <?php include("HTML/header.php"); ?>
+        <?php include("HTMLPHP/header.php"); ?>
            <p> Bonjour et bienvenue sur le catalogue RetroGaming, vous allez pouvoir retrouver les jeux de votre enfance! Et même le completer avec vos jeux préfèrés ! <br>
            Have fun ! </p>
 
-           <img id="retro" src="/images/retrogaming.jpg" alt="photo accueil"> >
+           <img id="retro" src="IMAGES/retrogaming.jpg" alt="photo accueil"> >
     </body>
 </html>
